d8f72dd3f182e9d06c9b67b059dce56bb93f25de
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-09-30 02:55:01 -0400

    Put the boilerplate copyright text in the code.

--- UXL-Launcher/OptionsWindow.vb
+++ UXL-Launcher/OptionsWindow.vb
@@ -1,3 +1,29 @@
-﻿Public Class OptionsWindow
+﻿'UXL Launcher - Launch Office Apps Quickly!
+'Copyright (C) 2013-2016  Drew Naylor
+'Microsoft Office is copyright Microsoft Corp.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of UXL Launcher
+'(Program is also known as "Unified eXecutable Launcher." Not to be confused with
+'another software titled "[Kindle] Unified Application Launcher".)
+'
+'UXL Launcher is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'UXL Launcher is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with Drew's App Launcher.  If not, see <http://www.gnu.org/licenses/>.
 
+
+
+Public Class OptionsWindow
+    Private Sub OptionsWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
