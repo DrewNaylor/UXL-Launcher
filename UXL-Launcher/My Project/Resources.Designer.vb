25d6c30c7918a8b6a9f5a4dd557d10698e2ab1f4
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-10-05 17:25:57 -0400

    Added the UXL icon to the VS project.
    Plus I added a comment to it.

--- UXL-Launcher/My Project/Resources.Designer.vb
+++ UXL-Launcher/My Project/Resources.Designer.vb
@@ -193,6 +193,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Public ReadOnly Property UXL_Standalone_Icon() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("UXL_Standalone_Icon", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Public ReadOnly Property Word() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Word", resourceCulture)
