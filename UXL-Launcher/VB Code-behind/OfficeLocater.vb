297491195912d8a30cc29346bd47df0378a102d0
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-11-26 03:49:32 -0500

    Adding a class for constructing a string
    based on My.Settings to properly launch Office on the user's computer.

---
+++ UXL-Launcher/VB Code-behind/OfficeLocater.vb
@@ -0,0 +1,27 @@
+﻿'UXL Launcher - Launchers for Microsoft Office apps in one place.
+'Copyright (C) 2013-2016  Drew Naylor
+'Microsoft Office and all related words are copyright
+'and trademark Microsoft Corporation.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of UXL Launcher
+'(Program is also known as "Unified eXecutable Launcher." Not to be confused with
+'another software titled "[Kindle] Unified Application Launcher".)
+'
+'UXL Launcher is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'UXL Launcher is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with UXL Launcher.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Public Class OfficeLocater
+
+End Class
