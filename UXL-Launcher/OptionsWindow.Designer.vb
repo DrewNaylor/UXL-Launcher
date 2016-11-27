49323328c06ac56b663dcc15e55eadfdf4d937ee
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-11-27 01:46:05 -0500

    Putting a button into the Options window.

--- UXL-Launcher/OptionsWindow.Designer.vb
+++ UXL-Launcher/OptionsWindow.Designer.vb
@@ -23,12 +23,15 @@ Partial Class aaformOptionsWindow
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.tableLayoutPanelOptionsWindow = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.tableLayoutPanelOptionsWindow.SuspendLayout()
         Me.SuspendLayout()
         '
         'tableLayoutPanelOptionsWindow
         '
         Me.tableLayoutPanelOptionsWindow.ColumnCount = 1
         Me.tableLayoutPanelOptionsWindow.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tableLayoutPanelOptionsWindow.Controls.Add(Me.Button1, 0, 1)
         Me.tableLayoutPanelOptionsWindow.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tableLayoutPanelOptionsWindow.Location = New System.Drawing.Point(0, 0)
         Me.tableLayoutPanelOptionsWindow.Name = "tableLayoutPanelOptionsWindow"
@@ -37,6 +40,18 @@ Partial Class aaformOptionsWindow
         Me.tableLayoutPanelOptionsWindow.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
         Me.tableLayoutPanelOptionsWindow.Size = New System.Drawing.Size(970, 788)
         Me.tableLayoutPanelOptionsWindow.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.Button1.AutoSize = True
+        Me.Button1.Location = New System.Drawing.Point(817, 720)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 3, 15, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 57)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'aaformOptionsWindow
         '
@@ -53,9 +68,12 @@ Partial Class aaformOptionsWindow
         Me.ShowInTaskbar = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Options"
+        Me.tableLayoutPanelOptionsWindow.ResumeLayout(False)
+        Me.tableLayoutPanelOptionsWindow.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents tableLayoutPanelOptionsWindow As TableLayoutPanel
+    Friend WithEvents Button1 As Button
 End Class
