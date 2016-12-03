c7dec0eb531462ae1c57f13eca195f902c076b71
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-03 14:50:28 -0500

    Putting controls in the real About window.

--- UXL-Launcher/RealAboutWindow.Designer.vb
+++ UXL-Launcher/RealAboutWindow.Designer.vb
@@ -25,7 +25,11 @@ Partial Class RealAboutWindow
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.buttonClose = New System.Windows.Forms.Button()
         Me.buttonUpdateCheck = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TableLayoutPanel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -37,6 +41,7 @@ Partial Class RealAboutWindow
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.buttonClose, 3, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.buttonUpdateCheck, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TabControl1, 0, 0)
         Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
@@ -68,6 +73,38 @@ Partial Class RealAboutWindow
         Me.buttonUpdateCheck.Text = "Check for Updates"
         Me.buttonUpdateCheck.UseVisualStyleBackColor = True
         '
+        'TabControl1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.TabControl1, 4)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(3, 3)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(528, 433)
+        Me.TabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(520, 407)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(122, 74)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
         'RealAboutWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
@@ -78,6 +115,7 @@ Partial Class RealAboutWindow
         Me.Text = "RealAboutWindow"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.TableLayoutPanel1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -85,4 +123,7 @@ Partial Class RealAboutWindow
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents buttonClose As Button
     Friend WithEvents buttonUpdateCheck As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
 End Class
