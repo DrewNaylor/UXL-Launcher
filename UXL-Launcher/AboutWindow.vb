a02769ac0ad5a273a5f7afa0d39af2e47cf60226
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-04 10:21:36 -0500

    More About textbox restructuring.

--- UXL-Launcher/AboutWindow.vb
+++ UXL-Launcher/AboutWindow.vb
@@ -39,16 +39,15 @@ App compiled at UTC" & BuildDateString &
 UXL Launcher provides launchers for most Microsoft Office apps in one place. 
 
 License notice:
-UXL Launcher is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+UXL Launcher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by" &
+"the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 UXL Launcher is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with UXL Launcher.  If not, see <http://www.gnu.org/licenses/>." & vbCrLf &
+"" &
 "I am not intending on infringing on Microsoft" & "'" & "s copyrights, so this is only a shortcut application.
 
 Copyright notice Office, Microsoft Office, Word, Excel, PowerPoint, And all related words (by MSFT) are Copyright [a long time ago]-[sometime in the future] Microsoft Corp. All Rights Reserved for Microsoft" & "'" & "s copyrights.")
