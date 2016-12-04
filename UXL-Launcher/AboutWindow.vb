a6888498fd9397462eab3379a5d7f727d2741992
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-04 16:54:39 -0500

    Fixed one About window paragraph.

--- UXL-Launcher/AboutWindow.vb
+++ UXL-Launcher/AboutWindow.vb
@@ -45,8 +45,7 @@ UXL Launcher is free software: you can redistribute it and/or modify it under th
 UXL Launcher is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with UXL Launcher.  If not, see <http://www.gnu.org/licenses/>." & vbCrLf &
+You should have received a copy of the GNU General Public License along with UXL Launcher.  If not, see <http://www.gnu.org/licenses/>." & vbCrLf &
 "" &
 "I am not intending on infringing on Microsoft" & "'" & "s copyrights, so this is only a shortcut application.
 
