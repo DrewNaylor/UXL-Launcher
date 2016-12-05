9d739dd5cf37e3a4dc5881b9225f78cc69eb4c36
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-05 12:02:52 -0500

    Clip Organizer and Query have error handlers.

--- UXL-Launcher/VB Code-behind/LaunchApp.vb
+++ UXL-Launcher/VB Code-behind/LaunchApp.vb
@@ -306,14 +306,64 @@ Public Class LaunchApp
 #End Region
 #Region "Microsoft Query Launcher Code."
     Public Shared Sub LaunchQuery()
-        ' Launch Microsoft Query.
-        Process.Start(OfficeLocater.fullLauncherCodeString & "MSQRY32.EXE")
+        ' Launch Microsoft Query. Try...Catch code source here: <http://www.homeandlearn.co.uk/NET/nets5p4.html>
+        Try
+            Process.Start(OfficeLocater.fullLauncherCodeString & "MSQRY32.EXE")
+        Catch ex As System.ComponentModel.Win32Exception
+            ' If Microsoft Query isn't found in the folder the user chose in the Options window, ask them if they want to
+            ' go to the Options window to change it.
+            Dim msgResult As Integer = MessageBox.Show("We couldn't find Microsoft Query in the location specified in the Options window." &
+            " Would you like to open the Options window to change your settings?", "Couldn't find file",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+
+            ' If the user chooses to open the Options window, open the Options window to the General tab.
+            If msgResult = DialogResult.Yes Then
+                Dim forceOptionsWindowTab As New aaformOptionsWindow
+                forceOptionsWindowTab.tabcontrolOptionsWindow.SelectTab(0)
+                forceOptionsWindowTab.ShowDialog()
+            End If
+        Catch ex As Exception
+            ' If another error shows up, then we can't handle it yet and ask the user if they want to file a
+            ' bug report.
+            Dim msgResult As Integer = MessageBox.Show("An error occurred that we can't handle yet." & vbCrLf &
+                "Would you like to file a bug report online?", "I just don't know what went wrong!",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+            ' If the user chooses to file a bug report online, go to the GitHub Issues "New Issue."
+            If msgResult = DialogResult.Yes Then
+                Process.Start("https://github.com/DrewNaylor/UXL-Launcher/issues/new")
+            End If
+        End Try
     End Sub
 #End Region
 #Region "Microsoft Clip Organizer Launcher Code."
     Public Shared Sub LaunchClipOrganizer()
-        ' Launch Microsoft Clip Organizer.
-        Process.Start(OfficeLocater.fullLauncherCodeString & "MSTORE.EXE")
+        ' Launch Microsoft Clip Organizer. Try...Catch code source here: <http://www.homeandlearn.co.uk/NET/nets5p4.html>
+        Try
+            Process.Start(OfficeLocater.fullLauncherCodeString & "MSTORE.EXE")
+        Catch ex As System.ComponentModel.Win32Exception
+            ' If Microsoft Clip Organizer isn't found in the folder the user chose in the Options window, ask them if they want to
+            ' go to the Options window to change it.
+            Dim msgResult As Integer = MessageBox.Show("We couldn't find Microsoft Clip Organizer in the location specified in the Options window." &
+            " Would you like to open the Options window to change your settings?", "Couldn't find file",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+
+            ' If the user chooses to open the Options window, open the Options window to the General tab.
+            If msgResult = DialogResult.Yes Then
+                Dim forceOptionsWindowTab As New aaformOptionsWindow
+                forceOptionsWindowTab.tabcontrolOptionsWindow.SelectTab(0)
+                forceOptionsWindowTab.ShowDialog()
+            End If
+        Catch ex As Exception
+            ' If another error shows up, then we can't handle it yet and ask the user if they want to file a
+            ' bug report.
+            Dim msgResult As Integer = MessageBox.Show("An error occurred that we can't handle yet." & vbCrLf &
+                "Would you like to file a bug report online?", "I just don't know what went wrong!",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+            ' If the user chooses to file a bug report online, go to the GitHub Issues "New Issue."
+            If msgResult = DialogResult.Yes Then
+                Process.Start("https://github.com/DrewNaylor/UXL-Launcher/issues/new")
+            End If
+        End Try
     End Sub
 #End Region
 #Region "Microsoft Picture Manager Launcher Code."
