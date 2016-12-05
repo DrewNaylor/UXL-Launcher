ca952ab8aabe1ab1b9c58146bfa87e9f7fb8193f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-05 12:06:40 -0500

    Picture Manager has error handler code.

--- UXL-Launcher/VB Code-behind/LaunchApp.vb
+++ UXL-Launcher/VB Code-behind/LaunchApp.vb
@@ -368,8 +368,33 @@ Public Class LaunchApp
 #End Region
 #Region "Microsoft Picture Manager Launcher Code."
     Public Shared Sub LaunchPictureManager()
-        ' Launch Microsoft Picture Manager.
-        Process.Start(OfficeLocater.fullLauncherCodeString & "OIS.EXE")
+        ' Launch Microsoft Picture Manager. Try...Catch code source here: <http://www.homeandlearn.co.uk/NET/nets5p4.html>
+        Try
+            Process.Start(OfficeLocater.fullLauncherCodeString & "OIS.EXE")
+        Catch ex As System.ComponentModel.Win32Exception
+            ' If Microsoft Picture Manager isn't found in the folder the user chose in the Options window, ask them if they want to
+            ' go to the Options window to change it.
+            Dim msgResult As Integer = MessageBox.Show("We couldn't find Microsoft Picture Manager in the location specified in the Options window." &
+            " Would you like to open the Options window to change your settings?", "Couldn't find file",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+
+            ' If the user chooses to open the Options window, open the Options window to the General tab.
+            If msgResult = DialogResult.Yes Then
+                Dim forceOptionsWindowTab As New aaformOptionsWindow
+                forceOptionsWindowTab.tabcontrolOptionsWindow.SelectTab(0)
+                forceOptionsWindowTab.ShowDialog()
+            End If
+        Catch ex As Exception
+            ' If another error shows up, then we can't handle it yet and ask the user if they want to file a
+            ' bug report.
+            Dim msgResult As Integer = MessageBox.Show("An error occurred that we can't handle yet." & vbCrLf &
+                "Would you like to file a bug report online?", "I just don't know what went wrong!",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Error)
+            ' If the user chooses to file a bug report online, go to the GitHub Issues "New Issue."
+            If msgResult = DialogResult.Yes Then
+                Process.Start("https://github.com/DrewNaylor/UXL-Launcher/issues/new")
+            End If
+        End Try
     End Sub
 #End Region
 #Region "Microsoft OneNote Quick Launch Launcher Code."
