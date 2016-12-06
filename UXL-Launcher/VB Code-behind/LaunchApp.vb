ce20c58b7923865d8556a6819abbc4a41c1fae8b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-12-06 14:13:33 -0500

    Add error message to Powerpoint.

--- UXL-Launcher/VB Code-behind/LaunchApp.vb
+++ UXL-Launcher/VB Code-behind/LaunchApp.vb
@@ -189,7 +189,9 @@ Public Class LaunchApp
             ' If Microsoft Powerpoint isn't found in the folder the user chose in the Options window, ask them if they want to
             ' go to the Options window to change it.
             Dim msgResult As Integer = MessageBox.Show("We couldn't find Microsoft PowerPoint in the location specified in the Options window." &
-            " Would you like to open the Options window to change your settings?", "Couldn't find file",
+            " Would you like to open the Options window to change your settings?" & vbCrLf &
+                "" & vbCrLf &
+                "Full error message: " & ex.Message, "Couldn't find file",
             MessageBoxButtons.YesNo, MessageBoxIcon.Error)
 
             ' If the user chooses to open the Options window, open the Options window to the General tab.
@@ -202,7 +204,9 @@ Public Class LaunchApp
             ' If another error shows up, then we can't handle it yet and ask the user if they want to file a
             ' bug report.
             Dim msgResult As Integer = MessageBox.Show("An error occurred that we can't handle yet." & vbCrLf &
-                "Would you like to file a bug report online?", "I just don't know what went wrong!",
+                "Would you like to file a bug report online?" & vbCrLf &
+                "" & vbCrLf &
+                "Full error message: " & ex.Message, "I just don't know what went wrong!",
             MessageBoxButtons.YesNo, MessageBoxIcon.Error)
             ' If the user chooses to file a bug report online, go to the GitHub Issues "New Issue."
             If msgResult = DialogResult.Yes Then
