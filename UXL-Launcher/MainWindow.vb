7770737a6d3e0cdccccdf531b39d4d9fad122009
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-01-29 00:46:34 -0500

    Move Help menu code region for buttons.

--- UXL-Launcher/MainWindow.vb
+++ UXL-Launcher/MainWindow.vb
@@ -136,9 +136,35 @@ Public Class aaformMainWindow
         "main window TopMost: " & Me.TopMost
 
     End Sub
-#End Region
-#End Region
+#Region "Help menubar buttons."
+    Private Sub menubarAboutButton_Click(sender As Object, e As EventArgs) Handles menubarAboutButton.Click
+        ' Open the About window to About tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
+        Dim forceAboutWindowTab As New aaformAboutWindow
+        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(0)
+        forceAboutWindowTab.ShowDialog()
+    End Sub
 
+    Private Sub menubarLicenseButton_Click(sender As Object, e As EventArgs) Handles menubarLicenseButton.Click
+        ' Open the About window to License tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
+        Dim forceAboutWindowTab As New aaformAboutWindow
+        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(1)
+        forceAboutWindowTab.ShowDialog()
+    End Sub
+
+    Private Sub menubarAuthorsButton_Click(sender As Object, e As EventArgs) Handles menubarAuthorsButton.Click
+        ' Open the About window to Acknowledgements tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
+        Dim forceAboutWindowTab As New aaformAboutWindow
+        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(2)
+        forceAboutWindowTab.ShowDialog()
+    End Sub
+
+    Private Sub menubarHelpTopicsButton_Click(sender As Object, e As EventArgs) Handles menubarHelpTopicsButton.Click
+        ' Go to the GitHub wiki.
+        Process.Start("https://github.com/DrewNaylor/UXL-Launcher/wiki")
+    End Sub
+#End Region
+#End Region
+#End Region
 
 #Region "App Launcher Code."
 
@@ -212,35 +238,7 @@ Public Class aaformMainWindow
         LaunchApp.LaunchOfficeLangPrefs()
     End Sub
 #End Region
-#Region "Help menubar buttons."
-    Private Sub menubarAboutButton_Click(sender As Object, e As EventArgs) Handles menubarAboutButton.Click
-        ' Open the About window to About tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
-        Dim forceAboutWindowTab As New aaformAboutWindow
-        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(0)
-        forceAboutWindowTab.ShowDialog()
-    End Sub
 
-    Private Sub menubarLicenseButton_Click(sender As Object, e As EventArgs) Handles menubarLicenseButton.Click
-        ' Open the About window to License tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
-        Dim forceAboutWindowTab As New aaformAboutWindow
-        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(1)
-        forceAboutWindowTab.ShowDialog()
-    End Sub
-
-    Private Sub menubarAuthorsButton_Click(sender As Object, e As EventArgs) Handles menubarAuthorsButton.Click
-        ' Open the About window to Acknowledgements tab. Credit goes to this SO answer: <http://stackoverflow.com/a/2513186>
-        Dim forceAboutWindowTab As New aaformAboutWindow
-        forceAboutWindowTab.tabcontrolAboutWindow.SelectTab(2)
-        forceAboutWindowTab.ShowDialog()
-    End Sub
-
-    Private Sub menubarHelpTopicsButton_Click(sender As Object, e As EventArgs) Handles menubarHelpTopicsButton.Click
-        ' Go to the GitHub wiki.
-        Process.Start("https://github.com/DrewNaylor/UXL-Launcher/wiki")
-    End Sub
-
-
-#End Region
 #Region "Taskbar notification area launcher buttons."
     Private Sub notifyiconWord_Click(sender As Object, e As EventArgs) Handles notifyiconWord.Click
         ' Run Microsoft Word.
