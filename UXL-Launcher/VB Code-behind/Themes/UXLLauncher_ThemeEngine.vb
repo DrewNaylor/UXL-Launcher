45e443e45f20ac1adaecccc27b6b50ec80c67580
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-06-16 19:38:43 -0400

    Overwrite menuitem bgcolor if it doesn't exist.

--- UXL-Launcher/VB Code-behind/Themes/UXLLauncher_ThemeEngine.vb
+++ UXL-Launcher/VB Code-behind/Themes/UXLLauncher_ThemeEngine.vb
@@ -305,6 +305,9 @@ Public Class UXLLauncher_ThemeEngine
                 ' If the element isn't a valid HTML color, just ignore it.
             Catch ex As Exception
             End Try
+        Else
+            ' If the element doesn't exist, overwrite it with the Default theme's value.
+            colorMenuItemBackColor = Color.FromKnownColor(KnownColor.Window)
         End If
 #End Region
 
