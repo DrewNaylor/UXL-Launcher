e96db7975a0a6d0d73880a6b34296298a6ee380b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-06-16 19:42:17 -0400

    Overwrite statuslabel bgcolor if it doesn't exist.

--- UXL-Launcher/VB Code-behind/Themes/UXLLauncher_ThemeEngine.vb
+++ UXL-Launcher/VB Code-behind/Themes/UXLLauncher_ThemeEngine.vb
@@ -332,6 +332,9 @@ Public Class UXLLauncher_ThemeEngine
                 ' If the element isn't a valid HTML color, just ignore it.
             Catch ex As Exception
             End Try
+        Else
+            ' If the element doesn't exist, overwrite it with the Default theme's value.
+            colorStatusLabelBackColor = Color.FromKnownColor(KnownColor.Transparent)
         End If
 #End Region
 
