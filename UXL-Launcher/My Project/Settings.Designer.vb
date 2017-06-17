4408c5885c37383d94e8033011eff0f2dbc8b463
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-06-17 00:01:42 -0400

    Add option to allow logging.

--- UXL-Launcher/My Project/Settings.Designer.vb
+++ UXL-Launcher/My Project/Settings.Designer.vb
@@ -161,6 +161,18 @@ Namespace My
                 Me("enableThemeEngine") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property allowLogging() As Boolean
+            Get
+                Return CType(Me("allowLogging"),Boolean)
+            End Get
+            Set
+                Me("allowLogging") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
