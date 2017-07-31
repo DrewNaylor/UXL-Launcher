eabf84c9756c562174673a1207f90a83920bc843
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-07-30 23:19:24 -0400

    Remove unneeded code from About window text...
    I honestly have no idea why I wrote Microsoft" & "'" & "s for the About
    window's text. Typing Microsoft's works just fine.

--- UXL-Launcher/AboutWindow.vb
+++ UXL-Launcher/AboutWindow.vb
@@ -66,7 +66,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY o
 "" & vbCrLf &
 "You should have received a copy of the GNU General Public License along with UXL Launcher. If not, see <http://www.gnu.org/licenses/>." & vbCrLf &
 "" & vbCrLf &
-"Drew is not intending on infringing on Microsoft" & "'" & "s copyrights, so UXL Launcher is only a shortcut application." & vbCrLf &
+"Drew is not intending on infringing on Microsoft's copyrights, so UXL Launcher is only a shortcut application." & vbCrLf &
 "" & vbCrLf &
 "Copyright notice: Office, Microsoft Office, Word, Excel, PowerPoint, And all related words (by MSFT) are Copyright [a long time ago]-[sometime in the future] Microsoft Corp. All Rights Reserved for Microsoft" & "'" & "s copyrights." & vbCrLf &
 "Any other companies mentioned own their respective copyrights/trademarks.")
