7771d8d92a04daa0bfcf6a78f5fd539721572a81
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-03-17 22:24:49 -0400

    Begin implementing "Hide When Minimized"...
    as mentioned in #47.

--- UXL-Launcher/My Project/Settings.Designer.vb
+++ UXL-Launcher/My Project/Settings.Designer.vb
@@ -173,6 +173,18 @@ Namespace My
                 Me("debugmodeShowThemeEngineOutput") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property hideWhenMinimized() As Boolean
+            Get
+                Return CType(Me("hideWhenMinimized"),Boolean)
+            End Get
+            Set
+                Me("hideWhenMinimized") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
