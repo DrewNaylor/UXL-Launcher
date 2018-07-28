15cdfb802d0500003091112f54d63ebf652cd8e7
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-07-28 15:10:55 -0400

    Update WPF boilerplate.

--- wpf-uxl/MainWindow.xaml.vb
+++ wpf-uxl/MainWindow.xaml.vb
@@ -1,3 +1,36 @@
-﻿Class MainWindow
+﻿' UXL Launcher (WPF test) - A test to see if UXL Launcher can be ported to WPF.
+' Copyright (C) 2017-2018 Drew Naylor
+' Microsoft Office and all related words are copyright
+' and trademark Microsoft Corporation.
+' Microsoft is not affiliated with either the UXL Launcher project or Drew Naylor
+' and does not endorse this software.
+' Any other companies mentioned own their respective copyrights/trademarks.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+' Please be aware that UXL Launcher is unofficial and not made by Microsoft.
+'
+' This file is part of UXL Launcher (WPF test)
+' (Program is also known as "Unified eXecutable Launcher." Not to be confused with
+' another software titled "[Kindle] Unified Application Launcher",
+' "UX Launcher" [an Android launcher], or "Ulauncher" [a Linux app launcher].)
+' Additionally, the main UXL Launcher project is found here:
+' https://github.com/DrewNaylor/UXL-Launcher
+'
+' UXL Launcher (WPF test) is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' UXL Launcher (WPF test) is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with UXL Launcher (WPF test).  If not, see <http://www.gnu.org/licenses/>. -->
+
+
+
+Class MainWindow
 
 End Class
