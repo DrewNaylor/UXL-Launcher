5c28bc0c8ab9b1c8fdca5bc5d88e692e0d3daa8d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-05 09:24:32 -0400

    Don't bypass configured location for all apps by default.

--- UXL-Launcher/My Project/Settings.Designer.vb
+++ UXL-Launcher/My Project/Settings.Designer.vb
@@ -260,7 +260,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
         Public Property bypassConfiguredLocationForAllApps() As Boolean
             Get
                 Return CType(Me("bypassConfiguredLocationForAllApps"),Boolean)
