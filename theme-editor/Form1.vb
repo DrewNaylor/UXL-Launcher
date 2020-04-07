380b85280c923ad0e7045d7c52a1a5aa41c3805f
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-07 00:03:07 -0400

    Trying to do properties and stuff for the two test controls.

--- theme-editor/Form1.vb
+++ theme-editor/Form1.vb
@@ -1,3 +1,42 @@
 ﻿Public Class Form1
+    Private Sub ComboBox1_TextChanged(sender As Object, e As EventArgs) Handles ComboBox1.TextChanged
+
+    End Sub
+End Class
+
+
+Public Class ControlProperties
+    Private _buttonBackColor As String = "Transparent"
+    Private _buttonForeColor As String = "ControlText"
+    Private _labelBackColor As String = "Transparent"
+    Private _labelForeColor As String = "ControlText"
+
+    Public Property buttonBackColor() As String
+        Get
+            Return _buttonBackColor
+        End Get
+        Set(value As String)
+            _buttonBackColor = value
+        End Set
+    End Property
+
+    Public Property buttonForeColor() As String
+        Get
+            Return _buttonForeColor
+        End Get
+        Set(value As String)
+            _buttonForeColor = value
+        End Set
+    End Property
+
+    Public Property labelBackColor() As String
+        Get
+            Return _labelBackColor
+        End Get
+        Set(value As String)
+            _labelBackColor = value
+        End Set
+    End Property
+
 
 End Class
