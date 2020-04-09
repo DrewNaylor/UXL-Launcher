76d4cb3dc12a8fea78565f0eba535f6205621a6e
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-08 20:22:53 -0400

    Use new theme properties for the XML document.

--- theme-editor/ThemeProcessor.vb
+++ theme-editor/ThemeProcessor.vb
@@ -25,44 +25,40 @@ Imports System.Xml
 Public Class ThemeProcessor
 
     Friend Shared Sub LoadTheme(themeInput As String, Optional isFilename As Boolean = True)
-        ' Create a new XmlDocument to load the theme into.
-        Dim themeSheet As XmlDocument = New XmlDocument()
-
         ' Try to load the theme. We'll assume it's a file first before
         ' loading it as an XmlDocument.
         If isFilename = True AndAlso System.IO.File.Exists(themeInput) Then
-            themeSheet.Load(themeInput)
+            ThemeProperties.themeSheet.Load(themeInput)
         End If
 
         ' Most of this is based on the PortableThemeEngine theme loading code.
         ' Create a namespace manager, and add a namespace to it.
-        Dim themeNamespaceManager As New XmlNamespaceManager(themeSheet.NameTable)
-        themeNamespaceManager.AddNamespace("uxl", "https://drewnaylor.github.io/xml")
+        ThemeProperties.themeNamespaceManager.AddNamespace("uxl", "https://drewnaylor.github.io/xml")
 
         ' Get the theme title from the XML file.
-        ThemeProperties.themeinfoTitle = NodeReader("Title", themeSheet, themeNamespaceManager, "(No Title specified)")
+        ThemeProperties.themeinfoTitle = NodeReader("Title", "(No Title specified)")
 
         ' Get theme description from XML file.
-        ThemeProperties.themeinfoDescription = NodeReader("Description", themeSheet, themeNamespaceManager, "(No Description specified)")
+        ThemeProperties.themeinfoDescription = NodeReader("Description", "(No Description specified)")
 
         ' Get theme version from XML file.
-        ThemeProperties.themeinfoVersion = NodeReader("Version", themeSheet, themeNamespaceManager, "(No Version specified)")
+        ThemeProperties.themeinfoVersion = NodeReader("Version", "(No Version specified)")
 
         ' Get theme author.
-        ThemeProperties.themeinfoAuthor = NodeReader("Author", themeSheet, themeNamespaceManager, "(No Author specified)")
+        ThemeProperties.themeinfoAuthor = NodeReader("Author", "(No Author specified)")
 
         ' Get theme engine version to use.
         ' The theme engine can benefit from this code, so it'll
         ' be put into there eventually.
         ' First check to see if it's a decimal and if it can be parsed.
-        If Decimal.TryParse(NodeReader("UseThemeEngineVersion", themeSheet, themeNamespaceManager, "(No UseThemeEngineVersion specified)"), CDec(1.01)) = True Then
+        If Decimal.TryParse(NodeReader("UseThemeEngineVersion", "(No UseThemeEngineVersion specified)"), CDec(1.01)) = True Then
             ' If it is a decimal, then cast it as one and check if it's less than 1.01.
-            If CDec(NodeReader("UseThemeEngineVersion", themeSheet, themeNamespaceManager, "(No UseThemeEngineVersion specified)")) < 1.01 Then
+            If CDec(NodeReader("UseThemeEngineVersion", "(No UseThemeEngineVersion specified)")) < 1.01 Then
                 ' If it is, then set the theme engine runtime version to 1.01.
                 ThemeProperties.themeinfoUseThemeEngineVersion = "1.01"
-            ElseIf CDec(NodeReader("UseThemeEngineVersion", themeSheet, themeNamespaceManager, "(No UseThemeEngineVersion specified)")) >= 1.01 Then
+            ElseIf CDec(NodeReader("UseThemeEngineVersion", "(No UseThemeEngineVersion specified)")) >= 1.01 Then
                 ' Otherwise, it's equal to or larger than 1.01, so just load whatever it is.
-                ThemeProperties.themeinfoUseThemeEngineVersion = NodeReader("UseThemeEngineVersion", themeSheet, themeNamespaceManager, "(No UseThemeEngineVersion specified)")
+                ThemeProperties.themeinfoUseThemeEngineVersion = NodeReader("UseThemeEngineVersion", "(No UseThemeEngineVersion specified)")
             End If
         Else
             ' If it can't be parsed as a decimal, then set the theme engine runtime version to 1.01.
@@ -70,23 +66,23 @@ Public Class ThemeProcessor
         End If
 
         ' Get button backcolor.
-        ThemeProperties.themecontrolButtonBackColor = NodeReader("Theme_Colors/Button/BackColor", themeSheet, themeNamespaceManager, "Transparent")
+        ThemeProperties.themecontrolButtonBackColor = NodeReader("Theme_Colors/Button/BackColor", "Transparent")
 
         ' Get button forecolor.
-        ThemeProperties.themecontrolButtonForeColor = NodeReader("Theme_Colors/Button/ForeColor", themeSheet, themeNamespaceManager, "ControlText")
+        ThemeProperties.themecontrolButtonForeColor = NodeReader("Theme_Colors/Button/ForeColor", "ControlText")
 
         ' Get button flatstyle.
-        ThemeProperties.themecontrolButtonFlatStyle = NodeReader("Theme_Colors/Button/FlatStyle", themeSheet, themeNamespaceManager, "Standard")
+        ThemeProperties.themecontrolButtonFlatStyle = NodeReader("Theme_Colors/Button/FlatStyle", "Standard")
         ' If the button flatstyle is neither Flat nor Standard, set it to Standard.
         If Not ThemeProperties.themecontrolButtonFlatStyle = "Flat" Or ThemeProperties.themecontrolButtonFlatStyle = "Standard" Then
             ThemeProperties.themecontrolButtonFlatStyle = "Standard"
         End If
 
         ' Get button flatstyle bordercolor.
-        ThemeProperties.themecontrolButtonFlatAppearanceBorderColor = NodeReader("Theme_Colors/Button/FlatAppearance/BorderColor", themeSheet, themeNamespaceManager, "Nothing")
+        ThemeProperties.themecontrolButtonFlatAppearanceBorderColor = NodeReader("Theme_Colors/Button/FlatAppearance/BorderColor", "Nothing")
 
         ' Get groupbox backcolor.
-        ThemeProperties.themecontrolGroupboxBackColor = NodeReader("Theme_Colors/GroupBox/BackColor", themeSheet, themeNamespaceManager, "Nothing")
+        ThemeProperties.themecontrolGroupboxBackColor = NodeReader("Theme_Colors/GroupBox/BackColor", "Nothing")
 
 
 
@@ -102,10 +98,10 @@ Public Class ThemeProcessor
 
     End Sub
 
-    Private Shared Function NodeReader(inputNode As String, inputThemeSheet As XmlDocument, inputThemeNamespaceManager As XmlNamespaceManager, defaultValue As String) As String
+    Private Shared Function NodeReader(inputNode As String, defaultValue As String) As String
         ' Get theme info and return it to the calling code.
-        If inputThemeSheet.SelectSingleNode("/UXL_Launcher_Theme/" & inputNode & "[1]", inputThemeNamespaceManager) IsNot Nothing Then
-            Return inputThemeSheet.SelectSingleNode("/UXL_Launcher_Theme/" & inputNode & "[1]", inputThemeNamespaceManager).InnerText
+        If ThemeProperties.themeSheet.SelectSingleNode("/UXL_Launcher_Theme/" & inputNode & "[1]", ThemeProperties.themeNamespaceManager) IsNot Nothing Then
+            Return ThemeProperties.themeSheet.SelectSingleNode("/UXL_Launcher_Theme/" & inputNode & "[1]", ThemeProperties.themeNamespaceManager).InnerText
         Else
             Return defaultValue
         End If
