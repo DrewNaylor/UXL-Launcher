addf3e0384f4508db287fcf9cf0024f837109f6b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-09 22:45:07 -0400

    Add class for holding theme properties and one for...
    loading TE2.x themes.
    This is something I got the idea for while working on the theme editor.

---
+++ PortableUXLLauncher_ThemeEngine/ThemeProperties.vb
@@ -0,0 +1,26 @@
+﻿'PortableThemeEngine - Theme engine based off the UXL Launcher Theme Engine.
+'Can be used with standard Windows Forms applications with a few small changes.
+'Copyright (C) 2019-2020 Drew Naylor. Licensed under Gnu GPLv3+.
+'Any companies mentioned own their respective copyrights/trademarks.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of PortableThemeEngine.
+'
+'PortableThemeEngine is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'PortableThemeEngine is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with PortableThemeEngine.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+Public Class ThemeProperties
+
+End Class
