d77d67d656f29095d9c443b40b5cf8689bdfe52b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-04-20 22:47:31 -0400

    Delete version compatibility and default values version...
    diff XML files since they're now unused.

--- PortableUXLLauncher_ThemeEngine/My Project/Resources.Designer.vb
+++ PortableUXLLauncher_ThemeEngine/My Project/Resources.Designer.vb
@@ -90,21 +90,6 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to &lt;DefaultValuesList&gt;
-        '''	&lt;Default For=&quot;StatusBar&quot; PropertyName=&quot;BackColor&quot;&gt;
-        '''		&lt;Diff RuntimeVersion=&quot;1.03&quot; Value=&quot;LiteralNothing&quot; /&gt;
-        '''		&lt;Diff RuntimeVersion=&quot;1.02&quot; Value=&quot;Control&quot; /&gt;
-        '''		&lt;Diff RuntimeVersion=&quot;1.01&quot; Value=&quot;Control&quot; /&gt;
-        '''	&lt;/Default&gt;
-        '''&lt;/DefaultValuesList&gt;.
-        '''</summary>
-        Friend ReadOnly Property DefaultValuesVersionDiff() As String
-            Get
-                Return ResourceManager.GetString("DefaultValuesVersionDiff", resourceCulture)
-            End Get
-        End Property
-        
-        '''<summary>
         '''  Looks up a localized string similar to &lt;UXL_Launcher_Theme&gt;
         '''  &lt;Title&gt;Eyesore 2&lt;/Title&gt;
         '''  &lt;Description&gt;Originally a test theme for UXL Launcher, Eyesore 2 is meant to be a spiritual successor to the Windows 3.1 Hotdog Stand theme, but worse.&lt;/Description&gt;
@@ -247,21 +232,6 @@ Namespace My.Resources
             Get
                 Dim obj As Object = ResourceManager.GetObject("UXL_Launcher_Banner", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized string similar to &lt;FeatureList&gt;
-        '''  &lt;Feature For=&quot;Button&quot; Property=&quot;FlatAppearance/BorderColor&quot; VersionIntroduced=&quot;1.03&quot; /&gt;
-        '''  
-        '''  &lt;!-- Example compatibility nodes: --&gt;
-        '''  &lt;!--&lt;Feature For=&quot;Button&quot; Property=&quot;BackColor&quot; VersionIntroduced=&quot;2.1&quot; /&gt;
-        '''  &lt;Feature For=&quot;Button&quot; Property=&quot;ForeColor&quot; VersionIntroduced=&quot;4.3&quot; /&gt;--&gt;
-        '''&lt;/FeatureList&gt;.
-        '''</summary>
-        Friend ReadOnly Property VersionCompatibility() As String
-            Get
-                Return ResourceManager.GetString("VersionCompatibility", resourceCulture)
             End Get
         End Property
     End Module
