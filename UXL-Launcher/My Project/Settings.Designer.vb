7db48cb36a0a4f977827fbf0b0d20ff6d9bd46ac
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-09-26 03:39:37 -0400

    Update settings.

--- UXL-Launcher/My Project/Settings.Designer.vb
+++ UXL-Launcher/My Project/Settings.Designer.vb
@@ -81,12 +81,12 @@ Namespace My
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
-        Public Property cpuIsSixtyFourBit() As Boolean
+        Public Property pathUsePFxEightySix() As Boolean
             Get
-                Return CType(Me("cpuIsSixtyFourBit"),Boolean)
+                Return CType(Me("pathUsePFxEightySix"),Boolean)
             End Get
             Set
-                Me("cpuIsSixtyFourBit") = value
+                Me("pathUsePFxEightySix") = value
             End Set
         End Property
         
@@ -104,7 +104,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("14")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("16nomsi")>  _
         Public Property userOfficeVersion() As String
             Get
                 Return CType(Me("userOfficeVersion"),String)
