0549be36c447a1fd683263bc8d207fe74e039491
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-01-26 19:36:26 -0500

    Add comments.

--- PortableUXLLauncher_ThemeEngine/PreThemedControls.vb
+++ PortableUXLLauncher_ThemeEngine/PreThemedControls.vb
@@ -17,12 +17,14 @@ Public Class PreThemedControls
             ThemeProperties.flatappearanceButtonMouseOverBackColor = Nothing
         End If
 
+        ' Assign the pre-themed button properties.
         PreThemedButton.BackColor = ThemeProperties.colorButtonBackColor
         PreThemedButton.ForeColor = ThemeProperties.colorButtonForeColor
         PreThemedButton.FlatStyle = ThemeProperties.flatstyleButtonFlatStyle
         'Windows.Forms.MessageBox.Show(ThemeProperties.flatstyleButtonFlatStyle.ToString)
         'Windows.Forms.MessageBox.Show(ThemeProperties.themeSheet.OuterXml.ToString)
         If PreThemedButton.FlatStyle = Windows.Forms.FlatStyle.Flat Then
+            ' If it's meant to be flat, do the flat appearance stuff.
             PreThemedButton.FlatAppearance.BorderColor = ThemeProperties.flatappearanceButtonBorderColor
             PreThemedButton.FlatAppearance.MouseDownBackColor = ThemeProperties.flatappearanceButtonMouseDownBackColor
             PreThemedButton.FlatAppearance.MouseOverBackColor = ThemeProperties.flatappearanceButtonMouseOverBackColor
